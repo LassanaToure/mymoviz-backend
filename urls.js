ef8e3a616b7f06dca862d0388084b90e0436e782
Toure Lassana <l.toure910@laposte.net>
2024-10-02 16:06:21 +0200

    test

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'http://mymoviz-backend-lkquvc61n-lassanas-projects.vercel.app/movies';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://mymoviz-front-42ot2liux-lassanas-projects.vercel.app/movies';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
