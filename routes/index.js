56fa01e357225560bed84253ef7f708f30da4c0f
Toure Lassana <l.toure910@laposte.net>
2024-10-02 16:24:07 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 require('dotenv').config();
 const fetch = require("node-fetch");
 
-const api ='d02976d6d55fc19e08f4b2d2c2c65254'
+// const api ='d02976d6d55fc19e08f4b2d2c2c65254'
 
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`)
       .then(response => response.json())
       .then(data => {
         
